9d4275c916df3c09bf2e07fd40db0e4eacfd7dc3
aix <651360107@qq.com>
2022-09-08 14:33:37 +0800

    aixc

--- Makefile
+++ Makefile
@@ -6,4 +6,4 @@ LDFLAGS := -ldflags "-X config.Version=${VERSION}"
 
 .PHONY: build
 build:
-	GOARCH=amd64 GOOS=linux go build -ldflags="-w -s"  -o  aixc main.go
+	GOARCH=amd64 GOOS=linux go build -ldflags="-w -s"  -o  aixc aixc.go
--- cmd/version.go
+++ cmd/version.go
@@ -14,7 +14,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of xc",
 	Long:  `All software has versions. This is aixc's`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Aixc Version: v1.0 -- HEAD")
+	Run: func(_ *cobra.Command, _ []string) {
+		fmt.Println("Aixc Version: v2.0 -- HEAD")
 	},
 }
